ef8fa03067191edbe3fe7b704267581ae7c7989f
xiaoxuncle <open_source@126.com>
2016-09-12 22:22:22 +0800

    改名工具

---
+++ multi_rename.py
@@ -0,0 +1,20 @@
+import os
+import re
+
+def rename(path, old, new):
+    path.replace('\\', '/')
+    filenames = os.listdir(path)
+    for filename in filenames:
+        new_name = re.sub(old, new, filename)
+        os.rename(os.path.join(path, filename), os.path.join(path, new_name))
+
+if __name__ == '__main__':
+    #目标文件路径
+    path = input('Path: ')
+    #欲修改的字符
+    old = input('Old: ')
+    #修改后的字符
+    new = input('New: ')
+    rename(path.replace('\\', '/'), old, new)
+    
+    
